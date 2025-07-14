311c2636d10123ea849d0baf414dc43730ecfdfb
bb8nR2D2 <bb8nr2d2kid@icloud.com>
2025-07-14 11:58:12 -0400

    alpha-0.0.1 Patch 9: Added Chisel

--- src/main/java/com/bb8nr2d2/liquidslib/item/ModItems.java
+++ src/main/java/com/bb8nr2d2/liquidslib/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.bb8nr2d2.liquidslib.item;
 
 import com.bb8nr2d2.liquidslib.LiquidsLib;
+import com.bb8nr2d2.liquidslib.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -13,6 +14,9 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> HOT_ROCK = ITEMS.register("hot_rock",
             () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> COPPER_CHISEL = ITEMS.register("copper_chisel",
+            () -> new ChiselItem(new Item.Properties().durability(640)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
